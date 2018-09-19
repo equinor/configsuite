ff4b51e58f4467f900b53fd573ac6ee4a2a4dd9a
Markus Sortland Dregi <markusdregi@gmail.com>
2018-09-19 20:53:22 +0200

    Add setup.py and travis configuration

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='type_name',
+    packages=['type_name'],
+    author='Software Innovation Bergen, Statoil ASA and TNO',
+    tests_require=['pytest'],
+    test_suite='tests',
+)
