105c3418f3e9b378e9df2ffad99d45a99a9585aa
Markus Sortland Dregi <markusdregi@gmail.com>
2018-09-20 21:28:04 +0200

    Update setup.py test dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setuptools.setup(
     name='type_name',
     packages=['type_name'],
     author='Software Innovation Bergen, Statoil ASA and TNO',
+    install_requires=['enum34'],
+    setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
 )
