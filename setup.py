31c3c60a558004a1b6672da81439498de90becad
Markus Fanebust Dregi <mfane@statoil.com>
2018-09-21 14:00:35 +0200

    Add decorator, enum34 and pytest-runner as dependencies

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setuptools.setup(
     name='protoconf',
     packages=['protoconf'],
     author='Software Innovation Bergen, Statoil ASA and TNO',
+    install_requires=['decorator', 'enum34'],
+    setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite='tests',
 )
