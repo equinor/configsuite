8a537d949e29ad1b4d978a826dd94d5d82c4284e
Markus Sortland Dregi <markusdregi@gmail.com>
2018-09-27 12:04:10 +0200

    Add support for missing keys and unknown keys errors

--- tests/test_types.py
+++ tests/test_types.py
@@ -98,3 +98,35 @@ class TestTypes(unittest.TestCase):
             err = config_suite.errors[0]
             self.assertIsInstance(err, protoconf.InvalidTypeError)
             self.assertEqual(('pet',), err.key_path)
+
+    def test_unknown_key(self):
+        raw_config = {
+            'name': 'Markus',
+            'favourite_food': 'bibimpap',
+            'pet': {
+                'name': 'Donkey Kong',
+                'favourite_food': 'bananas',
+            },
+        }
+        config_suite = _build_name_pet_config_suite(raw_config)
+
+        self.assertFalse(config_suite.valid)
+        self.assertEqual(1, len(config_suite.errors))
+        err = config_suite.errors[0]
+        self.assertIsInstance(err, protoconf.UnknownKeyError)
+        self.assertEqual((), err.key_path)
+
+    def test_missing_pet_name(self):
+        raw_config = {
+            'name': 'Markus',
+            'pet': {
+                'favourite_food': 'bananas',
+            },
+        }
+        config_suite = _build_name_pet_config_suite(raw_config)
+
+        self.assertFalse(config_suite.valid)
+        self.assertEqual(1, len(config_suite.errors))
+        err = config_suite.errors[0]
+        self.assertIsInstance(err, protoconf.MissingKeyError)
+        self.assertEqual(('pet',), err.key_path)
