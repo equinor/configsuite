1f7f7e263541e6e34339210d69757e47f812ad34
Markus Sortland Dregi <markusdregi@gmail.com>
2018-10-30 06:59:39 +0100

    Add support for missing keys and unknown keys errors

--- configsuite/validation_errors.py
+++ configsuite/validation_errors.py
@@ -2,7 +2,21 @@ import collections
 
 
 class ValidationErrorNames(object):
+    UNKNOWN_KEY = 'unknown_key'
+    MISSING_KEY = 'missing_key'
     INVALID_TYPE = 'invalid_type'
+
+
+UnknownKeyError = collections.namedtuple(
+    ValidationErrorNames.UNKNOWN_KEY,
+    ('msg', 'key_path')
+    )
+
+
+MissingKeyError = collections.namedtuple(
+    ValidationErrorNames.MISSING_KEY,
+    ('msg', 'key_path')
+    )
 
 
 InvalidTypeError = collections.namedtuple(
