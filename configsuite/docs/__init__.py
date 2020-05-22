711296dacdbda1e9b131d3596576a8a2816d495f
Markus Dregi <markusdregi@gmail.com>
2020-05-22 08:33:39 +0200

    Have docs generator deduce requiredness

--- configsuite/docs/__init__.py
+++ configsuite/docs/__init__.py
@@ -43,7 +43,11 @@ def generate(schema, level=0):
     if schema[MK.Type] == types.NamedDict:
 
         def req_child_marker(key):
-            return "*" if schema[MK.Content][key].get(MK.Required, True) else ""
+            child_req = not (
+                schema[MK.Content][key].get(MK.AllowNone, False)
+                or schema[MK.Content][key].get(MK.Default, None) is not None
+            )
+            return "*" if child_req else ""
 
         docs += [
             "\n".join(
